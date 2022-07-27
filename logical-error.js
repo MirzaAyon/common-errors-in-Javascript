397035247908ac261536ff22918d121767098edc
MirzaAyon <ayonmirza98@gmail.com>
2022-07-27 20:08:27 +0600

    some errors added

---
+++ logical-error.js
@@ -0,0 +1,4 @@
+//shob thik ache but ami logic e bhul korle eke bola hoy logicale bhul korle eke bole logical error
+//jemon 3+3 = 8 chaile eta logical error
+//eta ber korar way holo comment out kore run kore kore dekhbo
+//ager step e ki korsi ta check korbo evabe logcal error check kore 
